f745b3ce75c204bbb51602f7c90766fd3430f1c8
devremoto2 <adilson@almeidapedro.com.br>
2019-11-12 13:25:37 -0300

    code

---
+++ Algorithms/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Algorithms
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			FizzBuzz();
+			Console.WriteLine();
+
+			Fibonacci(100);
+			Console.WriteLine();
+
+			ChangeValue(19, 23);
+			Console.ReadLine();
+		}
+		public static void FizzBuzz()
+		{
+			for (int i = 1; i <= 100; i++)
+			{
+				if (i % 3 == 0 && i % 5 == 0)
+				{
+					Console.WriteLine("FizzBuzz");
+				}
+				else if (i % 3 == 0)
+				{
+					Console.WriteLine("Fizz");
+				}
+				else if (i % 5 == 0)
+				{
+					Console.WriteLine("Buzz");
+				}
+				else
+				{
+					Console.WriteLine(i);
+				}
+			}
+		}
+		/// <summary>
+		/// First n numbers of fibonacci
+		/// </summary>
+		/// <param name="n"></param>
+		public static void Fibonacci(int n)
+		{
+			long preivous = 0, current = 1, temp;
+
+			for (int i = 0; i < n; i++)
+			{
+				temp = preivous;
+				preivous = current;
+				current = preivous + temp;
+				Console.WriteLine($"{i} - {current}");
+
+			}
+		}
+		/// <summary>
+		/// Change the value of 3 varibles without a temp variable
+		/// </summary>
+		/// <param name="a"></param>
+		/// <param name="b"></param>
+		public static void ChangeValue(int a, int b)
+		{
+			Console.WriteLine($"Original values a {a} , b {b}");
+			a += b;
+			b = a - b;
+			a -= b;
+			Console.WriteLine($"Changed values a {a} , b {b}");
+		}
+	}
+
+}
